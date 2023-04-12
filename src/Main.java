4f2496c7a1b95db25461856a45b3a1af64df892c
Vinicius <113072217+ViniciusAndradee@users.noreply.github.com>
2023-04-11 22:09:08 -0300

    atualização do programa e otimização do codigo

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,60 @@
 import javax.swing.*;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        //Scanner leitor = new Scanner(System.in);
+        /*
+        Scanner leitor = new Scanner(System.in);
+        Produto produto = new Produto("ervilha", 5.50);
+        */
 
-        // Produto produto = new Produto("ervilha", 5.50);
         Produto produto;
-
-
 
         boolean encerrar = false;
 
         String nome = "";
+        int quantidade = 0;
         double valor = 0;
         double total = 0;
 
         do {
+            /*
             System.out.println("Nome do produto: ");
-            //nome = leitor.nextLine();
-            nome = JOptionPane.showInputDialog("");
+            nome = leitor.nextLine();
+            */
 
+            nome = JOptionPane.showInputDialog("Nome do produto: ");
+
+            /*
             System.out.println("Valor do produto [apenas números]");
-            //valor = leitor.nextDouble();
-            valor = Double.parseDouble(JOptionPane.showInputDialog("Valor do produto [apenas números]"));
+            valor = leitor.nextDouble();
+             */
 
-            produto = new Produto(nome, valor);
+            valor = Double.parseDouble(JOptionPane.showInputDialog("Valor do produto [apenas números]: "));
+            quantidade = Integer.parseInt(JOptionPane.showInputDialog("Qual a quantidade? "));
 
+            produto = new Produto(nome, quantidade, valor);
             System.out.println(produto);
 
-            total = total + produto.getValor();
+            total = total + (produto.getValor() * produto.getQuantidade());
 
-            System.out.println("Deseja mais alguma coisa?" +
-                    "[ 1 ] = SIM" +
-                    "[ 0 ] = NÃO");
+            /*
+            System.out.println("""
+                        Deseja mais alguma coisa?
+                        [ 1 ] = SIM
+                        [ 0 ] = NÃO
+                    """);
+            encerrar = leitor.hasNextInt();
+            var opcao = leitor.nextInt() ;
+            */
 
-            // encerrar = leitor.hasNextInt();
-            //var opcao = leitor.nextInt() ;
-            int opcao = Integer.parseInt(JOptionPane.showInputDialog("Deseja mais alguma coisa?"));
+            int opcao = Integer.parseInt(JOptionPane.showInputDialog("""
+                        Deseja mais alguma coisa?
+                        [ 1 ] = SIM
+                        [ 0 ] = NÃO
+                    """));
 
             encerrar = opcao == 1 ? false : true;
 
